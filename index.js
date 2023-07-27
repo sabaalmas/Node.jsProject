5073abb6d65d41a52de5ebffd2ee3b6f6654982d
Almas Saba <almassrm1234@gmail.com>
2023-07-27 14:41:43 -0400

    add public file for assets

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require ('path')
 const app = express()
-
+app.use(express.static('public'))
 
 app.get('/', (request,response)=>{
     //response.json({})
