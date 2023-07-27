e92e94cdef3e1d7602d9b78c8641ab6cdcdc3471
Almas Saba <almassrm1234@gmail.com>
2023-07-26 23:27:36 -0400

    add code to attach html files for all the pages

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const { status } = require("express/lib/response")
 const http= require("http")
 const { url } = require("inspector")
+
+const fs= require("fs")
+
+const homePage= fs.readFileSync("index.html")
+const contactPage= fs.readFileSync("contact.html")
+const aboutPage= fs.readFileSync("about.html")
+
+
 const server= http.createServer((request,response)=>{
     if(request.url==='/about'){
-
-         response.end("THE ABOUT PAGE...")
+        response.setHeader('Content-Type', 'text/html'); 
+        return  response.end(aboutPage)
         
     }
     else if(request.url==='/contact'){
-        response.end("THE CONTACT PAGE")
+        response.setHeader('Content-Type', 'text/html'); 
+       return  response.end(contactPage)
     }
     else if(request.url==='/'){
-         response.end('the home page...')
+        response.setHeader('Content-Type', 'text/html'); 
+        return response.end(homePage)
         
     }
     else{
        response.writeHead(404)
-       response.end("the page doesnt exist...")
+       response.end(homePage)
     }
 })
-server.listen(8000)
+server.listen(8080)
