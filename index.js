0494355b56189acca392586ef2e622b45f82b9ed
Almas Saba <almassrm1234@gmail.com>
2023-07-26 22:23:28 -0400

    set status code for pages

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+const { status } = require("express/lib/response")
 const http= require("http")
+const { url } = require("inspector")
 const server= http.createServer((request,response)=>{
-    console.log(request.url)
-    response.end("The server has been created .... ")
+    if(request.url==='/about'){
+
+         response.end("THE ABOUT PAGE...")
+        
+    }
+    else if(request.url==='/contact'){
+        response.end("THE CONTACT PAGE")
+    }
+    else if(request.url==='/'){
+         response.end('the home page...')
+        
+    }
+    else{
+       response.writeHead(404)
+       response.end("the page doesnt exist...")
+    }
 })
 server.listen(8000)
