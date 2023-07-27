6048635e310b03a1d8c129b9118f06ffcade3fea
Almas Saba <almassrm1234@gmail.com>
2023-07-27 01:10:18 -0400

    use express

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
-const { status } = require("express/lib/response")
-const http= require("http")
-const { url } = require("inspector")
+const express = require('express')
 
-const fs= require("fs")
+const app = express()
 
-const homePage= fs.readFileSync("index.html")
-const contactPage= fs.readFileSync("contact.html")
-const aboutPage= fs.readFileSync("about.html")
-
-
-const server= http.createServer((request,response)=>{
-    if(request.url==='/about'){
-        response.setHeader('Content-Type', 'text/html'); 
-        return  response.end(aboutPage)
-        
-    }
-    else if(request.url==='/contact'){
-        response.setHeader('Content-Type', 'text/html'); 
-       return  response.end(contactPage)
-    }
-    else if(request.url==='/'){
-        response.setHeader('Content-Type', 'text/html'); 
-        return response.end(homePage)
-        
-    }
-    else{
-       response.writeHead(404)
-       response.end(homePage)
-    }
+app.listen(3000,()=> {
+    console.log("the app is listening on port 3000")
 })
-server.listen(8080)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { status } = require("express/lib/response")
+// const http= require("http")
+// const { url } = require("inspector")
+
+// const fs= require("fs")
+
+// const homePage= fs.readFileSync("index.html")
+// const contactPage= fs.readFileSync("contact.html")
+// const aboutPage= fs.readFileSync("about.html")
+
+
+// const server= http.createServer((request,response)=>{
+//     if(request.url==='/about'){
+//         response.setHeader('Content-Type', 'text/html'); 
+//         return  response.end(aboutPage)
+        
+//     }
+//     else if(request.url==='/contact'){
+//         response.setHeader('Content-Type', 'text/html'); 
+//        return  response.end(contactPage)
+//     }
+//     else if(request.url==='/'){
+//         response.setHeader('Content-Type', 'text/html'); 
+//         return response.end(homePage)
+        
+//     }
+//     else{
+//        response.writeHead(404)
+//        response.end(homePage)
+//     }
+// })
+// server.listen(8080)
